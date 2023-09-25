24fe9bace90a0802a5bb175a9b510544d3e27832
ngahirinuki <hir23003@byui.edu>
2023-09-26 03:17:46 +1300

    add changes

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,16 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("This is in C#");
+
+        Console.Write($"What is your first name? ");
+        string firstname = Console.ReadLine();
+        
+        Console.Write($"What is your Last name? ");
+        string lastname = Console.ReadLine();
+        
+        Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}.");
+
+
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,39 @@
 using System;
 
 class Program
-{
-    static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        static void Main(string[] args)
+        {
+            Console.Write("What is your grade? ");
+            string userInput = Console.ReadLine();
+            int x = int.Parse(userInput);
+
+            if (x != 0 || userInput == "0")
+            {
+                if (x >= 90)
+                {
+                    Console.WriteLine("Your grade is an A");
+                }
+                else if (x >= 80)  
+                {
+                    Console.WriteLine("Your grade is a B");
+                }
+                else if (x >= 70)
+                {
+                    Console.WriteLine("Your grade is a C");
+                }
+                else if (x >= 60)
+                {
+                    Console.WriteLine("Your grade is a D");
+                }
+                else
+                {
+                    Console.WriteLine("You have failed");
+                }
+                        
+               
+            }
+            
+            
+        }
     }
-}
