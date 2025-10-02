b2ee470a8a580fcd15b03369d32543d62b51c0f9
ngahirinuki <hir23003@byui.edu>
2025-10-03 02:02:05 +1300

    Learning Activity 03

---
+++ week03/Fractions/Fraction.cs
@@ -0,0 +1,56 @@
+
+
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+
+    public Fraction()
+    {
+        _top = 0;
+        _bottom = 0;
+    }
+
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public int GetTop()
+    {
+        return _top;
+    }
+    public int SetTop(int top)
+    {
+        return _top = top;
+    }
+
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+    public int SetBottom(int bottom)
+    {
+        return _bottom = bottom;
+    }
+
+    public string GetFractionString()
+    {
+        return $"{_top} / {_bottom}";
+
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_top / _bottom;
+
+    }
+}
--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction f1 = new Fraction(); // using default settings
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
+
+        Fraction f2 = new Fraction(5); // using wholeNumber setting
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+
+        Fraction f3 = new Fraction(3, 4); // using top and bottom number
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+
+        Fraction f4 = new Fraction(1, 3); // using top and bottom number
+        Console.WriteLine(f4.GetFractionString());
+        Console.WriteLine(f4.GetDecimalValue());
     }
 }
